a8da2617759f1b047f2c77c77ec54ea2863a68b3
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-15 17:37:23 +0200

    created the models in models.py and changed the default user model in settings.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse(
+            "kitchen:cook-detail",
+            kwargs={"pk": self.pk},
+        )
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes",
+    )
+    cooks = models.ManyToManyField(
+        Cook,
+        related_name="dishes",
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            "kitchen:dish-detail",
+            kwargs={"pk": self.pk},
+        )
