c1bd6b68eef74a7245cf7a0ae659e7e6199f0e60
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-15 21:12:48 +0200

    added the models Cook, DishType, Dish to the admin

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from kitchen.models import Cook, Dish, DishType
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional information",
+            {
+                "fields": (
+                    "years_of_experience",
+                ),
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "years_of_experience",
+                ),
+            },
+        ),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "years_of_experience",
+        "is_staff",
+    )
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "dish_type",
+        "price",
+    )
+    list_filter = ("dish_type",)
+    search_fields = ("name",)
+    filter_horizontal = ("cooks",)
