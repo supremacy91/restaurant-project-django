98d111f0e9de949371d2f3bffe5b579dd557bba8
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-16 17:35:44 +0200

    added routing, view and template

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+
+
+def index(request):
+    context = {
+        "num_cooks": Cook.objects.count(),
+        "num_dishes": Dish.objects.count(),
+        "num_dish_types": DishType.objects.count(),
+    }
+
+    return render(
+        request,
+        "kitchen/index.html",
+        context=context,
+    )
