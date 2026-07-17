f1d496034640836ca543efb42995a6e4588978a3
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-17 14:59:30 +0200

    Added dish list and dish type list on the storefront rendering

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishListView, DishTypeListView
 
 app_name = "kitchen"
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list",
+    ),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Cook, Dish, DishType
 
@@ -15,3 +16,14 @@ def index(request):
         "kitchen/index.html",
         context=context,
     )
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 5
+    queryset = Dish.objects.select_related("dish_type")
