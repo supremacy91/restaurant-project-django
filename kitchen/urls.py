19e9d05cf5bfef5bf79bddec5ed178ef849c11a5
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-17 19:10:40 +0200

    Added CRUD for the dish types and dishes

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,19 +1,69 @@
 from django.urls import path
 
-from kitchen.views import index, DishListView, DishTypeListView
+from kitchen.views import (
+    CookListView,
+    DishCreateView,
+    DishDeleteView,
+    DishDetailView,
+    DishListView,
+    DishTypeCreateView,
+    DishTypeDeleteView,
+    DishTypeListView,
+    DishTypeUpdateView,
+    DishUpdateView,
+)
 
 app_name = "kitchen"
 
 urlpatterns = [
-    path("", index, name="index"),
     path(
         "dish-types/",
         DishTypeListView.as_view(),
         name="dish-type-list",
     ),
     path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create",
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update",
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete",
+    ),
+    path(
         "dishes/",
         DishListView.as_view(),
         name="dish-list",
+    ),
+    path(
+        "dishes/create/",
+        DishCreateView.as_view(),
+        name="dish-create",
+    ),
+    path(
+        "dishes/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail",
+    ),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update",
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete",
+    ),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list",
     ),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import generic
 
@@ -23,7 +24,48 @@ class DishTypeListView(generic.ListView):
     paginate_by = 5
 
 
+class DishTypeCreateView(generic.CreateView):
+    model = DishType
+    fields = ("name",)
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class DishTypeUpdateView(generic.UpdateView):
+    model = DishType
+    fields = ("name",)
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class DishTypeDeleteView(generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
 class DishListView(generic.ListView):
     model = Dish
     paginate_by = 5
     queryset = Dish.objects.select_related("dish_type")
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+
+class DishCreateView(generic.CreateView):
+    model = Dish
+    fields = "__all__"
+
+
+class DishUpdateView(generic.UpdateView):
+    model = Dish
+    fields = "__all__"
+
+
+class DishDeleteView(generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 5
