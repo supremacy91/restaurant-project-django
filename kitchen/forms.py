98b15f62a233314bbbbeef7f6da2edfd9b904b41
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-20 21:22:12 +0200

    Added search for Dishes, DIsh types and Cooks

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -4,6 +4,19 @@ from django.contrib.auth.forms import UserCreationForm
 from kitchen.models import Cook
 
 
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search...",
+            }
+        ),
+    )
+
+
 class CookCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Cook
--- kitchen/views.py
+++ kitchen/views.py
@@ -3,28 +3,55 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from kitchen.forms import CookCreationForm, CookUpdateForm
+from kitchen.forms import CookCreationForm, CookUpdateForm, SearchForm
 from kitchen.models import Cook, Dish, DishType
 
 
 
-def index(request):
-    context = {
-        "num_cooks": Cook.objects.count(),
-        "num_dishes": Dish.objects.count(),
-        "num_dish_types": DishType.objects.count(),
-    }
+# def index(request):
+#     context = {
+#         "num_cooks": Cook.objects.count(),
+#         "num_dishes": Dish.objects.count(),
+#         "num_dish_types": DishType.objects.count(),
+#     }
+#
+#     return render(
+#         request,
+#         "kitchen/index.html",
+#         context=context,
+#     )
 
-    return render(
-        request,
-        "kitchen/index.html",
-        context=context,
-    )
+
+class SearchMixin:
+    search_field = None
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        self.search_form = SearchForm(self.request.GET)
+
+        if self.search_form.is_valid():
+            query = self.search_form.cleaned_data["query"]
+
+            if query:
+                queryset = queryset.filter(
+                    **{f"{self.search_field}__icontains": query}
+                )
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["search_form"] = self.search_form
+
+        return context
 
 
-class DishTypeListView(LoginRequiredMixin, generic.ListView):
+class DishTypeListView(LoginRequiredMixin, SearchMixin, generic.ListView):
     model = DishType
     paginate_by = 5
+    search_field = "name"
 
 
 class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
@@ -44,11 +71,11 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
     success_url = reverse_lazy("kitchen:dish-type-list")
 
 
-class DishListView(LoginRequiredMixin, generic.ListView):
+class DishListView(LoginRequiredMixin, SearchMixin, generic.ListView):
     model = Dish
     paginate_by = 5
     queryset = Dish.objects.select_related("dish_type")
-
+    search_field = "name"
 
 class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
@@ -73,9 +100,10 @@ class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
 
-class CookListView(LoginRequiredMixin, generic.ListView):
+class CookListView(LoginRequiredMixin, SearchMixin, generic.ListView):
     model = Cook
     paginate_by = 5
+    search_field = "username"
 
 
 class CookDetailView(LoginRequiredMixin, generic.DetailView):
