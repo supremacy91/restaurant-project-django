b88c623bc82d493a3618797e89cf5065f17736d9
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-07-29 18:57:18 +0200

    Try to fix the issue with Render images apply

--- restaurantprojectdjango/urls.py
+++ restaurantprojectdjango/urls.py
@@ -1,15 +1,17 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.static import serve
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("kitchen.urls", namespace="kitchen")),
-]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT,
-)
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
